5687a34aa218566ff5bbc4fb4e1af1f28109065a
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-10-09 13:22:03 +0100

    fade in

--- src/OllieWasHere.tsx
+++ src/OllieWasHere.tsx
@@ -1,16 +1,35 @@
-import emoji from "./assets/ginger_nerd.png"
+import emoji from "./assets/ginger_nerd.png";
+
+import {useRef, useEffect} from "react";
 
 export const OllieWasHere = () => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    // trigger fade in after 5 minutes :troll:
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (ref.current) {
+                ref.current.style.opacity = "1";
+            }
+        }, 5 * 60 * 1000);
+
+        return () => clearTimeout(timeout);
+    });
+
     return (
-        <div style={{
-            display: "flex",
-            position: "absolute",
-            bottom: "1vh",
-            left: "1vw",
-            alignItems: "center",
-            gap: "0.25vw",
-            fontSize: "0.75vw",
-        }}
+        <div
+            ref={ref}
+            style={{
+                display: "flex",
+                position: "absolute",
+                bottom: "1vh",
+                left: "1vw",
+                alignItems: "center",
+                gap: "0.25vw",
+                fontSize: "0.75vw",
+                opacity: 0,
+                transition: "opacity 20s",
+            }}
              aria-hidden="true"
         >
             <span>ollie was here</span>
